fd5ca3f81ae687e94ce8b94cd0f945892092765c
auliaaa08 <ab.auliaabilqis@gmail.com>
2025-11-06 12:51:30 +0700

    Modifikasi 1 SearchNilai

--- SearchNilai06.java
+++ SearchNilai06.java
@@ -1,19 +1,35 @@
+import java.util.Scanner;
 public class SearchNilai06 {
+
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlah = sc.nextInt();
+
+        int[] arrNilai = new int[jumlah];
 
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
-                break;
-            }
+            System.out.print("Masukkan nilai ke-" + (i + 1) + " : ");
+            arrNilai[i] = sc.nextInt();
         }
 
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
-        
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        int posisi = -1;
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (arrNilai[i] == key) {
+                posisi = i;
+                break;
+            }
+
+        }
+        if (posisi != -1) {
+            System.out.println("\nNilai " + key + " ditemukan pada indeks ke-" + (posisi + 1));
+        } else {
+            System.out.println("\nNilai yang dicari tidak ditemukan.");
+        }
+
     }
 }
